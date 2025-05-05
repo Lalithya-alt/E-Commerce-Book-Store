edbacd4fbd7deeac00d9a5fd5a9a2bcd0081ed54
Hasalapera <hasalashe777@gmail.com>
2025-05-06 02:53:50 +0530

    alignments
    css

--- E-Commerce Mini Project/Pages/admin/userDetails.php
+++ E-Commerce Mini Project/Pages/admin/userDetails.php
@@ -9,6 +9,11 @@
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
+        <style>
+          .table-container{
+            padding-bottom: 200px;
+          }
+        </style>
     </head>
 
     <body>
