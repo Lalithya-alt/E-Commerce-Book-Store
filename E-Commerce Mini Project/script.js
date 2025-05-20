0fb093228d9d089b4a9793fe66454395902733da
Hasalapera <hasalashe777@gmail.com>
2025-05-20 17:43:47 +0530

    change some css

--- E-Commerce Mini Project/script.js
+++ E-Commerce Mini Project/script.js
@@ -133,4 +133,15 @@ function resetError() {
     errorEl.textContent = "";
 }
 
+// window.addEventListener('DOMContentLoaded', () => {
+//   window.addEventListener('scroll', function () {
+//     const header = document.querySelector('.header');
+//     if (window.scrollY > 50) {
+//       header.classList.add('shrink');
+//     } else {
+//       header.classList.remove('shrink');
+//     }
+//   });
+// });
+
 
